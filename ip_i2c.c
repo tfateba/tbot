d489d72e9992716b206e2701855fc26da2c0b8ba
Theodore Ateba <tf.ateba@gmail.com>
2016-11-08 07:11:12 +0100

    Correct the syntaxe to fit ChibiOS style.

--- ip_i2c.c
+++ ip_i2c.c
@@ -25,12 +25,13 @@
  * @fn    i2cReadRegisters
  * @brief Read a register or a block of registers from the sensor.
  *
- * @param[in] i2cp    Pointer to the i2c interface.
- * @param[in] addr    Slave address without R/W bit.
- * @param[in] reg     Pointer to the first register address to read.
- * @param[in] rxbuf   Pointer to the buffer to store the data readed.
- * @param[in] lenght  Size of data to read.
- * @retuen		msg			The result of the reading operation.
+ * @param[in] i2cp    pointer to the i2c interface
+ * @param[in] addr    slave address without R/W bit
+ * @param[in] reg     pointer to the first register address to read
+ * @param[in] rxbuf   pointer to the buffer to store the data readed
+ * @param[in] lenght  size of data to read
+ *
+ * @retuen		msg			the result of the reading operation
  */
 msg_t i2cReadRegisters(I2CDriver *i2cp, uint8_t addr,
     uint8_t *reg, uint8_t *rxbuf, uint8_t lenght)
@@ -48,12 +49,12 @@ msg_t i2cReadRegisters(I2CDriver *i2cp, uint8_t addr,
  * @fn    i2cWriteRegisters
  * @brief Write to a register or a block of registers on the sensor.
  *
- * @param[in] i2cp    Pointer to the i2c interface.
- * @param[in] addr    Slave address without R/W bit.
- * @param[in] txbuf   Pointer to the data to write into the sensor.
- *                    txbuf[0] is the first register to write.
- * @param[in] lenght  Size of data to write to the sensor.
- * @return		msg     The result of the reading operation.
+ * @param[in] i2cp    pointer to the i2c interface
+ * @param[in] addr    slave address without R/W bit
+ * @param[in] txbuf   pointer to the data to write into the sensor
+ *                    txbuf[0] is the first register to write
+ * @param[in] lenght  size of data to write to the sensor
+ * @return		msg     the result of the reading operation
  */
 msg_t i2cWriteRegisters(I2CDriver *i2cp, uint8_t addr,
     uint8_t *txbuf, uint8_t lenght)
