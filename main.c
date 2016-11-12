4a32f7f823eae11f738fd2f52d988ed7c430b092
Theodore Ateba <tf.ateba@gmail.com>
2016-11-12 23:16:57 +0100

    Clean the main file application.

--- main.c
+++ main.c
@@ -26,19 +26,15 @@
 #include "hal.h"
 #include "chprintf.h"
 // Local files
-#include "ip_adc.h"
 #include "ip_asserv.h"
-#include "ip_i2c.h"
-#include "ip_kalman.h"
-#include "ip_motor.h"
-#include "ip_mpu6050.h"
-#include "ip_pid.h"
-#include "ip_pwm.h"
 
 /*=========================================================================*/
 /* Application macros.                                                     */
 /*=========================================================================*/
+/* MPU6050 device name */
 #define mpu "MPU6050"
+/* Debug message activation. */
+#define DEBUG TRUE
 
 /*=========================================================================*/
 /* Global variables, I2C TX and RX buffers, I2C and Serial Configurations  */
@@ -46,45 +42,10 @@
 BaseSequentialStream* chp = (BaseSequentialStream*) &SD1; /*               */
 mpu6050_t       imu;        /**< MPU6050 instance.                         */
 msg_t           msg;        /**< Message error.                            */
-const uint8_t   delta = 10; /*                                             */
-
-bool    layingDown = true;
-double  targetAngle = 178; /**< The angle we want the robot to reach.      */
-double  targetOffset = 0;  /**< Offset for going forward and backwrd.      */
-double  turningOffset = 0; /**< Offset for turning left and right.         */
 
 /*=========================================================================*/
 /* Local functions.                                                        */
 /*=========================================================================*/
-
-void asservisment(void) {
-  msg = mpu6050_getData(&I2CD1, &imu);
-
-  imu.pitch = (atan2(imu.y_accel, imu.z_accel) + 3.14)*(180/3.14);
-  imu.pitch_k = kalman_getAngle(imu.pitch, (imu.x_gyro / 131.0), delta);
-
-  chprintf(chp, " pitch_kalman is %.3f °c\r\n", imu.pitch_k);
-
-  if ((layingDown && (imu.pitch_k < 170 || imu.pitch_k > 190)) ||
-    (!layingDown && (imu.pitch_k < 135 || imu.pitch_k > 225))) {
-      /*
-       * The robot is in a unsolvable position, so turn off both motors and
-       * wait until it's vertical again.
-       */
-      layingDown = true;
-      motorsStopAndReset();
-    } else {
-      /*
-       * It's no longer laying down,
-       * so we can try to stabilized the robot now.
-       */
-      layingDown = false;
-      pid(imu.pitch_k, targetAngle, targetOffset, turningOffset);
-    }
-
-    /* Update wheel velocity every 100ms. */
-    motorGetWheelVelocity();
-}
 
 /*=========================================================================*/
 /* Threads and main function.                                              */
@@ -101,8 +62,6 @@ static THD_FUNCTION(blinkThd, arg) {
 
   while (true) {
     palTogglePad(IOPORT2, PORTB_LED1);
-    // TODO: rmove the demo code.
-    //pwmDemo();
     chThdSleepMilliseconds(100);
   }
 }
@@ -118,7 +77,7 @@ static THD_FUNCTION(asserThd, arg) {
   chRegSetThreadName("Asservissement");
 
 	while (true) {
-		asservisment();
+		asserv();
     chThdSleepMilliseconds(10);
   }
 }
@@ -127,7 +86,6 @@ static THD_FUNCTION(asserThd, arg) {
  * Application entry point.
  */
 int main(void) {
-
   /*
    * System initializations.
    * - HAL initialization, this also initializes the configured device drivers
@@ -138,50 +96,102 @@ int main(void) {
   halInit();
   chSysInit();
 
-  /* Turn off the debug LED. */
-  palClearPad(IOPORT2, PORTB_LED1);
-
   /* Start the serial. */
   sdStart(&SD1, NULL);
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r");
+  chprintf(chp, "\n\r/*==============================================*/");
+  chprintf(chp, "\n\r/*                                              */");
+  chprintf(chp, "\n\r/*   Self  balancing robot.                     */");
+  chprintf(chp, "\n\r/*                                              */");
+  chprintf(chp, "\n\r/*    - Made by:   Theodore Ateba.              */");
+  chprintf(chp, "\n\r/*    - RTOS:      ChibiOS v16.1.5              */");
+  chprintf(chp, "\n\r/*    - Target:    Arduino Mega2560.            */");
+  chprintf(chp, "\n\r/*    - Version:   1.2                          */");
+  chprintf(chp, "\n\r/*    - Copyrigth: 2016.                        */");
+  chprintf(chp, "\n\r/*                                              */");
+  chprintf(chp, "\n\r/*==============================================*/");
+  chprintf(chp, "\n\r");
+  chprintf(chp, "\n\r Start Robot initialization:");
+  chprintf(chp, "\n\r Serial driver initialization ended.");
+  chThdSleepMilliseconds(3000);
+  #endif
+
+  /* Turn off the debug LED. */
+  palClearPad(IOPORT2, PORTB_LED1);
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r On-board LED initialization ended.");
+  chThdSleepMilliseconds(1000);
+  #endif
 
   /* Start I2C interface. */
   i2cStart(&I2CD1, &i2cConfig);
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r I2C bus interface initialization ended.");
+  chThdSleepMilliseconds(1000);
+  #endif
 
   /* Init Kalman filter. */
 	kalman_init();
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r Kalman filter initialization ended.");
+  chThdSleepMilliseconds(1000);
+  #endif
 
   /* Init MPU module. */
-	chprintf(chp, "\n\r MPU: Configurations started...");
   msg = mpu6050_init(&I2CD1, &imu, MPU6050_ADDR);
 
   if (msg != MSG_OK) {
-    chprintf(chp, "\n\r %s: Error while initialising the %s sensor.", mpu, mpu);
+    chprintf(chp, "\n\r Error while initialising the mpu sensor.");
     return -1;
   }
 
-  chprintf(chp, "\n\r MPU: Initialisation Ok.");
-  chprintf(chp, "\n\r MPU: Calibration, please wait while processing...");
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r IMU sensor initialization ended.");
+  chThdSleepMilliseconds(1000);
+  #endif
 
   /* Start MPU calibration process. */
   mpu6050_calibration(&I2CD1, &imu);
-  chprintf(chp, "\n\r MPU: Calibration ended.", msg);
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r IMU sensor calibration ended.");
+  chThdSleepMilliseconds(1000);
+  #endif
 
   /* Init Motors. */
   motorInit();
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r Motors initialization ended.");
+  chThdSleepMilliseconds(1000);
+  #endif
 
   /* Init PWM modules. */
   pwm_init();
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r PWM initialization ended.");
+  chThdSleepMilliseconds(1000);
+  #endif
 
-  //chThdSleepMilliseconds(1000);
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r Robot initialization end.");
+  chThdSleepMilliseconds(3000);
+  #endif
 
   /* Create and starts the LED blinker thread. */
   chThdCreateStatic(waBlink, sizeof(waBlink), NORMALPRIO, blinkThd, NULL);
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r Create Blink thread.");
+  chThdSleepMilliseconds(1000);
+  #endif
 
   /* Create and starts asservissement thread. */
   chThdCreateStatic(waAsser, sizeof(waAsser), NORMALPRIO+1, asserThd, NULL);
+  #if (DEBUG == TRUE)
+  chprintf(chp, "\n\r Create Asservissement thread.");
+  chThdSleepMilliseconds(1000);
+  #endif
 
   while (TRUE) {
     chThdSleepMilliseconds(1000);
   }
 }
-
