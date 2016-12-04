524cc0afe7fcd488fc931845474c0b57d4435d84
Theodore Ateba <tf.ateba@gmail.com>
2016-12-04 23:41:09 +0100

    Change the Serial Speed and activate the ADC driver

--- halconf.h
+++ halconf.h
@@ -48,7 +48,7 @@
  * @brief   Enables the ADC subsystem.
  */
 #if !defined(HAL_USE_ADC) || defined(__DOXYGEN__)
-#define HAL_USE_ADC                 FALSE
+#define HAL_USE_ADC                 TRUE
 #endif
 
 /**
@@ -294,7 +294,7 @@
  *          default configuration.
  */
 #if !defined(SERIAL_DEFAULT_BITRATE) || defined(__DOXYGEN__)
-#define SERIAL_DEFAULT_BITRATE      38400
+#define SERIAL_DEFAULT_BITRATE      115200
 #endif
 
 /**
