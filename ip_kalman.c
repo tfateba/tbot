4da93e836946b7368b0aa9f826919f10cd7badea
Theodore Ateba <tf.ateba@gmail.com>
2016-12-05 11:33:32 +0100

    Ajust the kalman filter for the robot.

--- ip_kalman.c
+++ ip_kalman.c
@@ -35,7 +35,7 @@ void kalman_init(void) {
   /* We will set the variables like so, these can also be tuned by the user */
   Q_angle   = 0.001;
   Q_bias    = 0.003;
-  R_measure = 0.03;
+  R_measure = 100;
   angle     = 0;      // Reset the angle
   bias      = 0;      // Reset bias
 
