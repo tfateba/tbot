b74492739b0a05944efc4084e19c0a303b6073b2
Theodore Ateba <tf.ateba@gmail.com>
2017-01-04 23:10:35 +0100

    Add robot configuration file.

---
+++ ip_conf.h
@@ -0,0 +1,27 @@
+/**
+ *
+ * @file    ip_conf.h
+ *
+ * @brief   Robot configuration header file.
+ *
+ * @author  Theodore Ateba, tf.ateba@gmail.com
+ *
+ * @date    08 December 2016
+ *
+ * @update  08 December 2016
+ *
+ */
+
+#ifndef _IP_CONF_H_
+#define _IP_CONF_H_
+
+/*===========================================================================*/
+/* Includes files.                                                           */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Enumerations, Structures and macros.                                      */
+/*===========================================================================*/
+#define DEBUG   FALSE       /* Debug message activation.                     */
+
+#endif /* _IP_CONF_H_ */
