648123ad2492aad7ff9ebe847d2db505049d2b69
Theodore ATEBA <tf.ateba@gmail.com>
2017-01-16 07:39:25 +0100

    Clean the ip_pid header file functions.

--- ip_pid.h
+++ ip_pid.h
@@ -8,7 +8,7 @@
  *
  * @date    13 July 2016
  *
- * @update  17 November 2016
+ * @update  16 January 2017
  *
  */
 
@@ -28,21 +28,7 @@
 /* Functions.                                                                */
 /*===========================================================================*/
 
-// TODO: Get the function prototypes from the c file.
-/**
- * @fn      pid
- * @brief   Calcul the command to send to the motors according to last error.
- *
- * @param[in] restAngle
- * @param[in] offset
- * @param[in] turning
- */
 void pid(double pitch, double restAngle, double offset, double turning);
-
-/**
- * @fn      pidParametersReset
- * @brief   Reset the PID parameters.
- */
 void pidParametersReset(void);
 
 #endif /* _IP_PID_H_ */
