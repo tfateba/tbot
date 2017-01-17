c7d1855dc7bc60cbda525d52768cf843864d202a
Theodore ATEBA <tf.ateba@gmail.com>
2017-01-17 07:32:02 +0100

    Remove the functions prototypes.

--- ip_i2c.h
+++ ip_i2c.h
@@ -34,33 +34,9 @@ static const I2CConfig i2cConfig = {
 /* Driver Functions                                                          */
 /*===========================================================================*/
 
-/**
- * @fn      i2cReadRegisters
- * @brief   Read a register or a block of registers from the sensor.
- *
- * @param[in] i2cp    pointer to the i2c interface
- * @param[in] addr    slave address without R/W bit
- * @param[in] reg     pointer to the first register address to read
- * @param[in] rxbuf   pointer to the buffer to store the data readed
- * @param[in] lenght  size of data to read
- *
- * @return    msg     the result of the reading operation
- */
 msg_t i2cReadRegisters(I2CDriver *i2cp, uint8_t addr,
     uint8_t *reg, uint8_t *rxbuf, uint8_t lenght);
 
-/**
- * @fn      i2cWriteRegisters
- * @brief   Write to a register or a block of registers on the sensor.
- *
- * @param[in] i2cp    pointer to the i2c interface
- * @param[in] addr    slave address without R/W bit
- * @param[in] txbuf   pointer to the data to write into the sensor
- *                    txbuf[0] is the first register to write
- * @param[in] lenght  size of data to write to the sensor
- *
- * @return    msg     the result of the reading operation
- */
 msg_t i2cWriteRegisters(I2CDriver *i2cp, uint8_t addr,
     uint8_t *txbuf, uint8_t lenght);
 
