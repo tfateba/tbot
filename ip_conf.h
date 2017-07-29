6e5048fbc772d4160b3d15ea91ff199a7b2b7903
Theodore <tf.ateba@gmail.com>
2017-07-30 00:46:25 +0200

    Add a debug macros for each file. */

--- ip_conf.h
+++ ip_conf.h
@@ -1,3 +1,4 @@
+
 /**
  *
  * @file    ip_conf.h
@@ -17,6 +18,15 @@
 /* Enumerations, Structures and macros.                                     */
 /*==========================================================================*/
 
-#define DEBUG   FALSE       /* Debug message activation.                    */
+#define DEBUG     FALSE   /**< Debug activation in all source files.        */
+#define DEBUG_MAI FALSE   /**< Debug activation in main file.               */
+#define DEBUG_ASS FALSE   /**< Debug activation in asserv file              */
+#define DEBUG_PID FALSE   /**< Debug activation in pid file.                */
+#define DEBUG_MOT FALSE   /**< Debug activation in Motor file.              */
+#define DEBUG_KAL FALSE   /**< Debug activation in Kalman filter file.      */
+#define DEBUG_PWM FALSE   /**< Debug activation in PWM file.                */
+#define DEBUG_I2C FALSE   /**< Debug activation in I2C file.                */
+#define DABUG_MPU FALSE   /**< Debug activation in MPU6050 file.            */
 
 #endif /* IP_CONF_H */
+
