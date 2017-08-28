5bf761fcd53e8dfe3a4ad8bb16ddec0839fb1077
Theodore <tf.ateba@gmail.com>
2017-08-28 07:34:55 +0200

    Clean asservissement source and header file.

--- ip_asserv.c
+++ ip_asserv.c
@@ -1,3 +1,4 @@
+
 /**
  *
  * @file    ip_asserv.c
@@ -8,55 +9,71 @@
  *
  * @date    07 Septembre 2015
  *
- * @update  04 January 2017
- *
  */
 
-/*===========================================================================*/
-/* Includes files.                                                           */
-/*===========================================================================*/
-#include "ip_asserv.h"
+/*==========================================================================*/
+/* Includes files.                                                          */
+/*==========================================================================*/
 
-/*===========================================================================*/
-/* Application macros.                                                       */
-/*===========================================================================*/
-/* MPU6050 device name */
-#define mpu         "MPU6050"
+/* Standard files. */
+#include <math.h>
+#include <stdint.h>
+
+/* ChibiOS files. */
+#include "hal.h"
+#include "chprintf.h"
+
+/* Project local files. */
+#include "ip_conf.h"
+#include "ip_kalman.h"
+#include "ip_motor.h"
+#include "ip_mpu6050.h"
+#include "ip_pid.h"
+#include "ip_pwm.h"
+
+/*==========================================================================*/
+/* Application macros.                                                      */
+/*==========================================================================*/
+
 #define PI          3.14159265359
 #define RAD_TO_DEG  180/PI
 
-/*===========================================================================*/
-/* Global variables, I2C TX and RX buffers, I2C and Serial Configurations    */
-/*===========================================================================*/
-#if (DEBUG == TRUE)
-extern BaseSequentialStream* chp; /*                                         */
+/*==========================================================================*/
+/* Global variables, I2C TX and RX buffers, I2C and Serial Configurations   */
+/*==========================================================================*/
+
+/* Local variables. */
+bool    layingDown    = true;     /**< Robot position, down or not.         */
+double  targetAngle   = 180;      /**< The angle we want the robot to reach.*/
+double  targetOffset  = 0;        /**< Offset for going forward and backwrd.*/
+double  turningOffset = 0;        /**< Offset for turning left and right.   */
+
+const double   dt = 0.01;         /**< Asservissement period.               */
+
+/* Extern variables. */
+#if (DEBUG == TRUE || DEBUG_ASS == TRUE)
+extern BaseSequentialStream* chp; /*                                        */
 #endif
-extern mpu6050_t       imu;       /**< MPU6050 instance.                     */
-extern msg_t           msg;       /**< Message error.                        */
 
-const double   dt = 0.01;       /**< Asservissement period.              */
+extern mpu6050_t       imu;       /**< MPU6050 instance.                    */
+extern msg_t           msg;       /**< Message error.                       */
 
-bool    layingDown    = true; /**<                                           */
-double  targetAngle   = 180;  /**< The angle we want the robot to reach.     */
-double  targetOffset  = 0;    /**< Offset for going forward and backwrd.     */
-double  turningOffset = 0;    /**< Offset for turning left and right.        */
-
-/*===========================================================================*/
-/* Functions.                                                                */
-/*===========================================================================*/
+/*==========================================================================*/
+/* Functions.                                                               */
+/*==========================================================================*/
 
 /**
- * @fn     asserv
  * @brief  Asservissement routine of the robot.
  */
 void asserv(void) {
 
   /* Read the IMU data (x,y,z accel and gyroscope). */
-  msg = mpu6050_getData(&I2CD1, &imu);
+  msg = mpu6050GetData(&I2CD1, &imu);
 
   if (msg != MSG_OK) {
-#if (DEBUG == TRUE)
-    chprintf(chp, "\n\r %s: Error while reading the %s sensor data.", mpu, mpu);
+#if (DEBUG == TRUE || DEBUG_ASS == TRUE)
+    chprintf(chp, "\n\r %s: Error while reading the MPU6050 sensor data.",
+    __func__);
 #endif
     return;
   }
@@ -65,7 +82,7 @@ void asserv(void) {
   imu.pitch = (atan2(imu.y_accel, imu.z_accel) + PI)*(RAD_TO_DEG);
 
   /* Get the Kalman estimation of the angle. */
-  imu.pitch_k = kalman_getAngle(imu.pitch, (imu.x_gyro / 131.0), dt);
+  imu.pitch_k = kalmanGetAngle(imu.pitch, (imu.x_gyro / 131.0), dt);
 
   if ((layingDown && (imu.pitch_k < 170 || imu.pitch_k > 190)) ||
     (!layingDown && (imu.pitch_k < 135 || imu.pitch_k > 225))) {
@@ -73,6 +90,10 @@ void asserv(void) {
      * The robot is in a unsolvable position, so turn off both motors and
      * wait until it's vertical again.
      */
+
+#if (DEBUG == TRUE || DEBUG_ASS == TRUE)
+    chprintf(chp, "%s: The Robot is laying down.\n\r", __func__);
+#endif
 
     layingDown = true;
     motorsStopAndReset();
@@ -84,8 +105,9 @@ void asserv(void) {
      */
     layingDown = false;
     pid(imu.pitch_k, targetAngle, targetOffset, turningOffset);
-#if (DEBUG == TRUE)
-    chprintf(chp, " pitch:%.3f\r\n", imu.pitch_k);
+#if (DEBUG == TRUE || DEBUG_ASS == TRUE)
+    chprintf(chp, "%s: pitch:%i\r\n", __func__, (uint8_t)imu.pitch_k);
+    //chprintf(chp, " pitch:%.3f\r\n", imu.pitch_k);
 #endif
   }
 
