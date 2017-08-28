b0d3ac8dc04d0e5dcc6820e52c641b3f9ef4c15f
Theodore <tf.ateba@gmail.com>
2017-08-28 07:13:14 +0200

    Rename a function and add comment. Clean the file.

--- ip_pid.c
+++ ip_pid.c
@@ -1,85 +1,93 @@
+
 /**
  * 
  * @file    ip_pid.c
  *
- * @brief   PID controller
+ * @brief   PID corrector source file.
  *
  * @author  Theodore Ateba, tf.ateba@gmail.com
  *
  * @date    17 July 2016
  *
- * @update  24 October 2016
- *
  */
 
-/*===========================================================================*/
-/* Includes files.                                                           */
-/*===========================================================================*/
-#include "ip_pid.h"
+/*==========================================================================*/
+/* Includes files.                                                          */
+/*==========================================================================*/
 
-/*===========================================================================*/
-/* Global variables.                                                         */
-/*===========================================================================*/
-static double lastError;    /**< Store position error.                       */
-static double iTerm;        /**< Store integral term                         */
-static double error;        /**< Error between two measurement               */
-static double pTerm;        /**< Proportionnal error.                        */
-static double dTerm;        /**< Derivate error.                             */
-static double PIDValue;     /**< PID value, sum of all the errors.           */
-static double PIDLeft;      /**< PID result for Left motor.                  */
-static double PIDRight;     /**< PID result for Right motor.                 */
+/* Standard files. */
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-static double Kp = 55.468;  /**< Proportional parameter of PID corrector.    */
-static double Ki = 0.554;   /**< Integral parameter of PID corrector.        */
-static double Kd = 42.524;  /**< Derivate parameter of PID corrector.        */
+/* Project local files. */
+#include "ip_motor.h"
 
-static double velocityScaleStop = 30;    /**< TODO: comment                  */
-static double velocityScaleTurning = 35; /**< TODO: comment                  */
+/*==========================================================================*/
+/* Global variables.                                                        */
+/*==========================================================================*/
 
-static int16_t zoneA = 4000;          /**< TODO: comment                     */
-static int16_t zoneB = 2000;          /**< TODO: comment                     */
-static double positionScaleA = 250;   /**< One resolution is 464 pulses      */
-static double positionScaleB = 500;   /**< TODO: comment                     */
-static double positionScaleC = 1000;  /**< TODO: comment                     */
-static double velocityScaleMove = 35; /**< TODO: comment                     */
+/* Local variables. */
+static double lastError;    /**< Store position error.                      */
+static double iTerm;        /**< Store integral term.                       */
+static double error;        /**< Error between two measurement.             */
+static double pTerm;        /**< Proportionnal error.                       */
+static double dTerm;        /**< Derivate error.                            */
+static double pidValue;     /**< PID value, sum of all the errors.          */
+static double pidLeft;      /**< PID result for Left motor.                 */
+static double pidRight;     /**< PID result for Right motor.                */
 
-extern long wheelPosition;      /**< TODO: comment                           */
-extern long lastWheelPosition;  /**< TODO: comment                           */
-extern long wheelVelocity;      /**< TODO: comment                           */
-extern long targetPosition;     /**< TODO: comment                           */
+static double kp = 55.468;  /**< Proportional parameter of PID corrector.   */
+static double ki = 0.554;   /**< Integral parameter of PID corrector.       */
+static double kd = 42.524;  /**< Derivate parameter of PID corrector.       */
 
-bool steerForward;          /**< TODO: comment                               */
-bool steerBackward;         /**< TODO: comment                               */
-bool steerStop      = true; /* Stop by default                               */
-bool steerLeft;             /**< TODO: comment                               */
-bool steerRight;            /**< TODO: comment                               */
+static double velocityScaleStop     = 30; /**< Max velocity of the robot.   */
+static double velocityScaleTurning  = 35; /**< Max turning velocity.        */
 
-/*===========================================================================*/
-/* Functions.                                                                */
-/*===========================================================================*/
+static int16_t zoneA = 4000;          /**< Area to ajust robot PID.         */
+static int16_t zoneB = 2000;          /**< Area to ajust robot PID.         */
+static double positionScaleA = 250;   /**< One resolution is 464 pulses.    */
+static double positionScaleB = 500;   /**< Max position scale for control.  */
+static double positionScaleC = 1000;  /**< Max position scale for control.  */
+static double velocityScaleMove = 35; /**< Velocity scale use to move.      */
+
+bool steerForward;          /**< Robot oriantation forward.                 */
+bool steerBackward;         /**< Robot oriantation backward.                */
+bool steerStop      = true; /* Stop by default                              */
+bool steerLeft;             /**< Robot orientation left.                    */
+bool steerRight;            /**< Robot oriantation rigth.                   */
+
+/* Extern variables. */
+extern long wheelPosition;
+extern long lastWheelPosition;
+extern long wheelVelocity;
+extern long targetPosition;
+
+/*==========================================================================*/
+/* Functions.                                                               */
+/*==========================================================================*/
 
 /**
- * @fn      pid
  * @brief   Calcul the command to send to the motors according to last error.
  *
- * @param[in] pitch     mesured angle of the robot
- * @param[in] restAngle TODO: comment
- * @param[in] offset    TODO: comment
- * @param[in] turning   TODO: comment
- */
+ * @param[in] pitch      mesured angle of the robot
+ * @param[in] restAngle  target angle of the robot
+ * @param[in] offset     angle we want to add to the target angle
+ * @param[in] turning    value use to turn robot over rigth or left
+*/
 void pid(double pitch, double restAngle, double offset, double turning) {
 
   /* Steer robot forward. */
   if (steerForward) {
     /* Scale down offset at high speed and scale up when reversing */
-    offset += (double)wheelVelocity/velocityScaleMove;
+    offset    += (double)wheelVelocity/velocityScaleMove;
     restAngle -= offset;
   }
 
   /* Steer robot backward. */
   else if (steerBackward) {
     /* Scale down offset at high speed and scale up when reversing */
-    offset -= (double)wheelVelocity/velocityScaleMove;
+    offset    -= (double)wheelVelocity/velocityScaleMove;
     restAngle += offset;
   }
 
@@ -100,12 +108,12 @@ void pid(double pitch, double restAngle, double offset, double turning) {
   }
 
   /* Update PID values. */
-  error = (restAngle - pitch);
-  pTerm = Kp * error;
-  iTerm += Ki * error;
-  dTerm = Kd * (error - lastError);
-  lastError = error;
-  PIDValue = pTerm + iTerm + dTerm;
+  error     =  (restAngle - pitch);
+  pTerm     =  kp * error;
+  iTerm     += ki * error;
+  dTerm     =  kd * (error - lastError);
+  lastError =  error;
+  pidValue  =  pTerm + iTerm + dTerm;
 
   /* Steer robot sideways. */
   if (steerLeft) {
@@ -113,44 +121,45 @@ void pid(double pitch, double restAngle, double offset, double turning) {
     turning -= abs((double)wheelVelocity/velocityScaleTurning);
     if (turning < 0)
       turning = 0;
-    PIDLeft = PIDValue-turning;
-    PIDRight = PIDValue+turning;
+    pidLeft   = pidValue-turning;
+    pidRight  = pidValue+turning;
   }
   else if (steerRight) {
     /* Scale down at high speed */
     turning -= abs((double)wheelVelocity/velocityScaleTurning);
     if (turning < 0)
       turning = 0;
-    PIDLeft = PIDValue+turning;
-    PIDRight = PIDValue-turning;
+    pidLeft   = pidValue+turning;
+    pidRight  = pidValue-turning;
   }
   else {
-    PIDLeft = PIDValue;
-    PIDRight = PIDValue;
+    pidLeft   = pidValue;
+    pidRight  = pidValue;
   }
 
+  // TODO: Remove the call to the motor here, it must be in a thread eg: in the asserv thread.
+
   /* Set the left motor PWM value. */
-  if (PIDLeft >= 0)
-    moveMotor(MOTOR_L, MOTOR_DIR_F, PIDLeft);
+  if (pidLeft >= 0)
+    motorMove(MOTOR_L, MOTOR_DIR_F, pidLeft);
   else
-    moveMotor(MOTOR_L, MOTOR_DIR_B, abs(PIDLeft));
+    motorMove(MOTOR_L, MOTOR_DIR_B, abs(pidLeft));
 
   /* Set the rigth motor PWM value. */
-  if (PIDRight >= 0)
-    moveMotor(MOTOR_R, MOTOR_DIR_F, PIDRight);
+  if (pidRight >= 0)
+    motorMove(MOTOR_R, MOTOR_DIR_F, pidRight);
   else
-    moveMotor(MOTOR_R, MOTOR_DIR_B, abs(PIDRight));
+    motorMove(MOTOR_R, MOTOR_DIR_B, abs(pidRight));
 }
 
 /**
- * @fn      pidParametersReset
  * @brief   Reset the PID parameters.
  */
-void pidParametersReset(void) {
-  pTerm     = 0;
-  iTerm     = 0;
-  dTerm     = 0;
-  lastError = 0;
-  targetPosition = wheelPosition;
+void pidResetParameters(void) {
+  pTerm           = 0;
+  iTerm           = 0;
+  dTerm           = 0;
+  lastError       = 0;
+  targetPosition  = wheelPosition;
 }
 
