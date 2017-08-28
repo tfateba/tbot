cfff15e116f69a4c9d086b24322011f901a7c20a
Theodore <tf.ateba@gmail.com>
2017-08-28 07:32:14 +0200

    Add encoder header file.

---
+++ ip_encoder.h
@@ -0,0 +1,48 @@
+
+/**
+ *
+ * @file    ip_encoder.h
+ *
+ * @brief   Encoder header file.
+ *
+ * @author  Theodore Ateba, tf.ateba@gmail.com
+ *
+ * @date    27 August 2017
+ *
+ */
+
+#ifndef IP_ENCODER_H
+#define IP_ENCODER_H
+
+/* Arduino Interruption */
+#define INT2 2      /**< D19 [PD2], BLACK wire motor Right                  */
+#define INT3 3      /**< D18 [PD3], RED wire Motor Left                     */
+
+#define L_ENCODER_A_PORT  IOPORT4 /**< Left encoder A port.                 */
+#define L_ENCODER_B_PORT  IOPORT7 /**< Left encoder B port.                 */
+
+#define L_ENCODER_A       PD3     /**< Left encoder A pin.                  */
+#define L_ENCODER_B       PG5     /**< Left encoder B pin.                  */
+
+#define R_ENCODER_B_PORT  IOPORT5 /**< Right encoder B port.                */
+#define R_ENCODER_A_PORT  IOPORT4 /**< Rigth encoder A port.                */
+
+#define R_ENCODER_A       PD2     /**< Right encoder A pin.                 */
+#define R_ENCODER_B       PE3     /**< Right encoder B pin.                 */
+
+
+/*==========================================================================*/
+/* Functions prototypes.                                                    */
+/*==========================================================================*/
+
+long readLeftEncoder(void);
+long readRightEncoder(void);
+long readLeftEncoderStateA(void);
+long readLeftEncoderStateB(void);
+long readRightEncoderStateA(void);
+long readRightEncoderStateB(void);
+void encoderInit(void);
+void motorGetWheelVelocity(void);
+
+#endif /* IP_ENCODER_H */
+
